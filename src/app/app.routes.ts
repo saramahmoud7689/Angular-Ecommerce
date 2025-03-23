c94aaa19b8691eb13c061de549a370d57c5e0f57
maria <tota.samuel21@gmail.com>
2025-03-23 17:33:34 +0200

    register form basic and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path:'',redirectTo:'home',pathMatch:'full'
+  },
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+   },
+   {
+    path: 'logout',
+    component: LogoutComponent
+   },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+
+];
+
